ace8ce0a09ae25ab169e144a2781f884522ea26c
ZakariyaeOuazzanix <zakariyaeouazzani90@gmail.com>
2024-07-23 12:45:16 +0100

    just before adding history to the tic tac toe game

--- src/App.js
+++ src/App.js
@@ -12,17 +12,28 @@ function Square({value, onSquareClick}){
   
 }
 
+
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true); 
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [TheWinner, setTheWinner] = useState(null)
 
   function handleClick(square){
-    const nextSquares = squares.slice();
-    nextSquares[square] = "X";
-    setSquares(nextSquares);
+    if(!squares[square] && !TheWinner){
+      const nextSquares = squares.slice();
+      if(xIsNext) nextSquares[square] = "X";
+      else nextSquares[square] = "O";
+      setSquares(nextSquares);
+      setXIsNext(!xIsNext);
+      CalculateWinner(nextSquares)
+      if (TheWinner) console.log(`The winner is ${TheWinner}`);
+    }
   }
 
   return (
     <>
+      <div className="status"> { !TheWinner ? ("Next player :" + (xIsNext ? "X" : "O")) : (`Winner : ${TheWinner}`) }  </div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -38,6 +49,34 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
+
+      <button onClick={reset} className="reset-btn">Reset</button>
     </>
   );
+
+  function reset(){
+    setSquares(Array(9).fill(null));
+    setTheWinner(null);
+    console.clear();
+  }
+
+  function CalculateWinner(squares) {
+    const winLines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+
+    for (let i in winLines){
+      const [a,b,c] = winLines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+          setTheWinner(squares[a]);
+        }
+    }
+  }
 }
