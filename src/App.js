8e5e2b15808cc35402d4f4c2102f3ca2d2fc6b58
ZakariyaeOuazzanix <zakariyaeouazzani90@gmail.com>
2024-07-23 17:06:55 +0100

    Almost done with the game, i still need to check if the moves are correct ...

--- src/App.js
+++ src/App.js
@@ -16,24 +16,77 @@ function Square({value, onSquareClick}){
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true); 
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const [TheWinner, setTheWinner] = useState(null)
+  const [TheWinner, setTheWinner] = useState(null);
+  const [history, SetHistory] = useState([Array(9).fill(null)]); /*empty array as the first value*/
+  const [moveItems, SetMoveItems] = useState([]);
+
+
+  function MovesList(){
+    /*loop over the moveItems and display them*/
+    return (
+      history.map((Board, idx) => {
+        if(idx === 0){
+          <li key={idx}>
+            <button onclick={setSquares(Array(9).fill(null))}>Go to game start</button>
+          </li>
+        }
+        <li key={idx}>
+          <button onclick={
+            () =>{
+              setSquares(Board)
+              if(idx %2 ==0) setXIsNext(true)
+              else setXIsNext(false);
+            }
+            }>
+              Go to move #{idx}</button>
+        </li>
+      })
+    );
+    }
+
+    /* if he moves while the board isn't at its latest state, we should erase the states before the current one in history, and therefore the movesList
+    will update accordingly, and we should check for xIsNext (the board we are on is it pair or unpair) 
+    */ 
+
 
   function handleClick(square){
+
+    if(isBoardEmpty()){
+      if(history.length == 1) createGoToGameStartBtn();
+      else {
+        const newHistory = history.slice(0,1);
+        SetHistory(newHistory);
+      }
+    }
+
+
     if(!squares[square] && !TheWinner){
+
+      if(squares !== history[history.length -1]){
+        const lastIdxToKeep = history.indexOf(squares);
+        const newHistory = history.slice(0,lastIdxToKeep +1);
+      }
+
       const nextSquares = squares.slice();
       if(xIsNext) nextSquares[square] = "X";
       else nextSquares[square] = "O";
+      const newHistory = [...history, nextSquares]
+      SetHistory(newHistory);
+      if(!isBoardEmpty()) addMove();
       setSquares(nextSquares);
       setXIsNext(!xIsNext);
-      CalculateWinner(nextSquares)
+      CalculateWinner(nextSquares);
       if (TheWinner) console.log(`The winner is ${TheWinner}`);
     }
+
   }
 
   return (
     <>
-      <div className="status"> { !TheWinner ? ("Next player :" + (xIsNext ? "X" : "O")) : (`Winner : ${TheWinner}`) }  </div>
-
+  <div className="game">
+        
+    <div>
+        <div className="status"> { !TheWinner ? ("Next player :" + (xIsNext ? "X" : "O")) : (`Winner : ${TheWinner}`) }  </div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -51,6 +104,16 @@ export default function Board() {
       </div>
 
       <button onClick={reset} className="reset-btn">Reset</button>
+    </div>
+
+    <div className="game-info">
+      <ol id="moves-list">
+        <MovesList />
+      </ol>
+    </div>
+
+  </div>
+
     </>
   );
 
@@ -78,5 +141,36 @@ export default function Board() {
           setTheWinner(squares[a]);
         }
     }
+  }
+
+  function addMove(){
+
+    const movesList =  document.getElementById('moves-list');
+    const newItem = document.createElement('li');
+    const button = document.createElement('button');
+    const lastIdx = history.length -1
+    button.textContent = `Go to move #${lastIdx}`;
+    button.onclick = () => {
+      setSquares(history[lastIdx]);
+    };
+    newItem.appendChild(button);
+    movesList.appendChild(newItem);
+
+  }
+
+  function isBoardEmpty(){
+    return JSON.stringify(squares)  === JSON.stringify(Array(9).fill(null)) ;
+  }
+
+  function createGoToGameStartBtn(){
+    const btn = document.createElement('button');
+    btn.onclick = () => {
+      setSquares(Array(9).fill(null));
+    };
+    btn.textContent = "Go to Game Start";
+    const firstItem = document.createElement('li');
+    firstItem.appendChild(btn);
+    const movesList = document.getElementById('moves-list');
+    movesList.appendChild(firstItem);
   }
 }
